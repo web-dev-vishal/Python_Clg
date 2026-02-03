ce6b8b64252eee55545a27157c003ebb433c21ce
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-03 23:19:37 +0530

    first code added Done

--- first.py
+++ first.py
@@ -1,44 +1,44 @@
-# Command line arguments: sum, average, maxixmum, minimum
+# Command line arguments: sum, average, maximum, minimum
 
 import sys
 
-print("Command Line Argumnets")
+print("Command Line Arguments")
 print("-"*40)
 
-if len(sys.argv) ==6:
+if len(sys.argv) == 6:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
     num3 = int(sys.argv[3])
     num4 = int(sys.argv[4])
     num5 = int(sys.argv[5])
  
-total = num1 + num2 + num3 + num4 + num5
-average = total / 5
+    total = num1 + num2 + num3 + num4 + num5
+    average = total / 5
 
-maxixmum = num1
-if num2 > maxixmum:
-    maxixmum = num2
-if num3 > maxixmum:
-    maxixmum = num3
-if num4 > maxixmum:
-    maxixmum = num4
-if num5 > maxixmum:
-    maxixmum = num5
+    maximum = num1
+    if num2 > maximum:
+        maximum = num2
+    if num3 > maximum:
+        maximum = num3
+    if num4 > maximum:
+        maximum = num4
+    if num5 > maximum:
+        maximum = num5
 
-minimum = num1
-if num2 > minimum:
-    minimum = num2
-if num3 > minimum:
-    minimum = num3
-if num4 > minimum:
-    minimum = num4
-if num5 > minimum:
-    minimum = num5
+    minimum = num1
+    if num2 < minimum:
+        minimum = num2
+    if num3 < minimum:
+        minimum = num3
+    if num4 < minimum:
+        minimum = num4
+    if num5 < minimum:
+        minimum = num5
 
-print("Sum", total)
-print("Average", average)
-print("Maximum", maxixmum)
-print("Minimum", minimum)
+    print("Sum:", total)
+    print("Average:", average)
+    print("Maximum:", maximum)
+    print("Minimum:", minimum)
 else:
     print("Please provide 5 numbers")
 
