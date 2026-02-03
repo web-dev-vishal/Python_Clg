f70fe62d7035a8ae6a4a39a4623551efdadd6a82
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-03 21:57:40 +0530

    Update in first code

--- first.py
+++ first.py
@@ -19,7 +19,7 @@ maxixmum = num1
 if num2 > maxixmum:
     maxixmum = num2
 if num3 > maxixmum:
-    maxixmum = nu3
+    maxixmum = num3
 if num4 > maxixmum:
     maxixmum = num4
 if num5 > maxixmum:
@@ -29,7 +29,7 @@ minimum = num1
 if num2 > minimum:
     minimum = num2
 if num3 > minimum:
-    minimum = nu3
+    minimum = num3
 if num4 > minimum:
     minimum = num4
 if num5 > minimum:
@@ -41,4 +41,50 @@ print("Maximum", maxixmum)
 print("Minimum", minimum)
 else:
     print("Please provide 5 numbers")
+
 print()
+
+# # Command line arguments: sum, average, maxixmum, minimum
+
+# import sys
+
+# print("Command Line Argumnets")
+# print("-"*40)
+
+# if len(sys.argv) ==6:
+#     num1 = int(sys.argv[1])
+#     num2 = int(sys.argv[2])
+#     num3 = int(sys.argv[3])
+#     num4 = int(sys.argv[4])
+#     num5 = int(sys.argv[5])
+ 
+# total = num1 + num2 + num3 + num4 + num5
+# average = total / 5
+
+# maxixmum = num1
+# if num2 > maxixmum:
+#     maxixmum = num2
+# if num3 > maxixmum:
+#     maxixmum = num3
+# if num4 > maxixmum:
+#     maxixmum = num4
+# if num5 > maxixmum:
+#     maxixmum = num5
+
+# minimum = num1
+# if num2 > minimum:
+#     minimum = num2
+# if num3 > minimum:
+#     minimum = num3
+# if num4 > minimum:
+#     minimum = num4
+# if num5 > minimum:
+#     minimum = num5
+
+# print("Sum", total)
+# print("Average", average)
+# print("Maximum", maxixmum)
+# print("Minimum", minimum)
+# else:
+#     print("Please provide 5 numbers")
+# print() solved the error in same logic and do the double check of given code 
