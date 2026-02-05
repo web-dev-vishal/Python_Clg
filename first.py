63d1f1e30d90b70004465f230d87eddba39c4b9d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-05 23:17:20 +0530

    code added with output

--- first.py
+++ first.py
@@ -11,3 +11,14 @@ print("Sum =", a+b+c+d+e)
 print("Average =", (a+b+c+d+e)/5)
 print("Maximum =", max(nums))
 print("Minimum =", min(nums))
+
+''' output
+Enter number 1: 23
+Enter number 2: 45
+Enter number 3: 89
+Enter number 4: 64
+Enter number 5: 24
+Sum = 245
+Average = 49.0
+Maximum = 89
+Minimum = 23 '''
