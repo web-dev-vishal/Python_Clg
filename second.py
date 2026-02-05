fcd46ea0875704e8e8f683524195d18ab855f9ab
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-02-05 23:21:42 +0530

    second code added

--- second.py
+++ second.py
@@ -1,1 +1,12 @@
 # Q2. Membership Operator
+
+numbers = [10, 20, 30, 40]
+
+x = int(input("Enter number: "))
+
+if x in numbers:
+    print("Number found in list")
+else:
+    numbers.append(x)
+    print("Number added")
+    print(numbers)
